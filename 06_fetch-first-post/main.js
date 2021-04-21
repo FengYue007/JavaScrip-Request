0672dd7b13b59bff7bf3915c39cd735b74217564
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-21 12:17:59 +0800

    first_jason fetch

--- 06_fetch-first-post/main.js
+++ 06_fetch-first-post/main.js
@@ -1,1 +1,24 @@
 // let's start coding:
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://pastleo-posts-api.herokuapp.com/api/posts')
+    .then(request => request.json())
+    .then(posts => { 
+    console.log(posts)
+
+    const firstPost = posts[0];
+
+    const postTitleA = document.querySelector('.post-title')
+
+    document.querySelector('.post-title').textContent = firstPost.title
+    document.querySelector('.post-created-at').textContent = firstPost.created_at
+    document.querySelector('.post-author').textContent = firstPost.author
+    document.querySelector('.post-description').textContent = firstPost.description    
+
+    const post = document.querySelector('.post-link')
+    postLinkA.classList.remove('hidden')
+    postLinkA.href = firstPost.url;
+   })
+})
+
+
