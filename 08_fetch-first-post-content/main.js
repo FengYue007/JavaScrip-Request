44902948fa45f28492e1711a8b6479f0a3f17fa0
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-21 15:31:04 +0800

    .then

--- 08_fetch-first-post-content/main.js
+++ 08_fetch-first-post-content/main.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const descriptionDiv = document.querySelector('.post-description');
   fetch('https://pastleo-posts-api.herokuapp.com/api/posts')
     .then(response => response.json())
+      return response.json()
+    // .then(function(response)){
+    //    returnresponse.json().then(posts => {
+    //      console.log(posts)
+    //    })
+    //  })
     .then(posts => {
       const post = posts[0];
       document.querySelector('.post-created-at').textContent = post.created_at;
@@ -8,10 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
       const postTitleA = document.querySelector('a.post-title');
       postTitleA.textContent = post.title;
       postTitleA.href = post.url;
-      const descriptionDiv = document.querySelector('.post-description');
+
       descriptionDiv.textContent = post.description;
 
       // let's start coding:
+      return fetch(post.api.url)
+    })
+    .then(response => response.json())
+    .then(fullPost => {
+      document.querySelector('.post-content').textContent = fullPost.content
+      descriptionDiv.classList.add('hidden')
     })
 });
+
 
