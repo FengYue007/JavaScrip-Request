60e131f9bea351325fd4cf559041bdd7817362a6
xiao-tang <10761101@gm.nfu.edu.tw>
2021-04-21 10:35:07 +0800

    first commit

---
+++ 09_create-post/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-post-form');
+  const submitBtn = form.querySelector('button[type=submit]');
+
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    // let's start coding
+
+  });
+})
